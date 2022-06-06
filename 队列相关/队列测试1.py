2b45628a6405acbe6edfb0a8aadd612d1b621f1b
chenym <loseyyou@gmail.com>
2022-06-06 16:46:10 +0800

    Changes

--- 队列相关/队列测试1.py
+++ 队列相关/队列测试1.py
@@ -1,28 +1,30 @@
 import threading
 import time
 
-def write_to_queue(queue):
+
+def write_to_queue(queue1):
     for index in range(5):
-        print('write {} to {}'.format(str(index), queue))
-        queue.put(index)
+        print('write {} to {}'.format(str(index), queue1))
+        queue1.put(index)
         time.sleep(1)
 
 
-def read_from_queue(queue):
+def read_from_queue(queue1):
     while True:
-        result = queue.get(True)
-        print('get {} from {}'.format(str(result), queue))
+        result = queue1.get(True)
+        print('get {} from {}'.format(str(result), queue1))
+
+
 if __name__ == '__main__':
     print('main thread is {}'.format(threading.current_thread().name))
     start_time = time.time()
-    # multithread queue
     from queue import Queue
 
     queue = Queue()
     tw = threading.Thread(target=write_to_queue, args=(queue,))
     tr = threading.Thread(target=read_from_queue, args=(queue,))
     # tr.setDaemon=True 设置为守护线程,当写入进程停止时，读进程也随之停止
-    tr.daemon=True
+    tr.daemon = True
     tw.start()
     tr.start()
     tw.join()
