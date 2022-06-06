d9ddd483a013578b9f5fff521565d7e9080b25ba
chenym <loseyyou@gmail.com>
2022-06-06 16:38:49 +0800

    Changes

--- test1.py
+++ test1.py
@@ -23,16 +23,18 @@ def read_from_queue(queue):
         result = queue.get(True)
         print('get {} from {}'.format(str(result), queue))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     # 父进程创建Queue，并传给各个子进程：
     q = Queue()
-    pw = Process(target=write, args=(q,))
-    pr = Process(target=read, args=(q,))
+    pw = Process(target=write_to_queue, args=(q,))
+    pr = Process(target=read_from_queue, args=(q,))
     # 启动子进程pw，写入:
+    pr.daemon = True
     pw.start()
     # 启动子进程pr，读取:
     pr.start()
     # 等待pw结束:
     pw.join()
     # pr进程里是死循环，无法等待其结束，只能强行终止:
-    pr.terminate()
+    # pr.terminate()
